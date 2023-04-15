fd8726ba15ba0fc257dd4810985956b7db458f88
unknown <mahamuda3052@gmail.com>
2023-04-15 23:00:39 +0600

    delete some button style

--- src/Pages/Home/DownloadBanar/DownloadBanar.js
+++ src/Pages/Home/DownloadBanar/DownloadBanar.js
@@ -7,7 +7,7 @@ const DownloadBanar = () => {
 <h1 className='text-center pt-[8.75rem] text-[2.5rem] text-[#FFFFFF] font-bold tracking-[0.2px] leading-[3.56rem]'>Download the app now.</h1>
 <p className='text-center text-2xl text-[#FFFFFF] font-medium tracking-[0.2px] leading-[2.37rem]'>Available on your favorite store. Start your premium experience now</p>
 {/* button */}
-<div className='flex lg:flex-row  justify-center  gap-4 mt-12 mx-auto '>
+<div className='md:flex flex-row  justify-center  gap-4 mt-12 mx-auto '>
 <button className=' text-[#FFFFFF]  bg-[#FA4A0C] rounded-[0.62rem] font-bold leading-7 text-base tracking-[0.2px] px-[1.93rem] py-[1.12rem]'>Play store</button>
 <button className='text-[#FFFFFF]  border-[0.06rem] border-solid border-[#FFFFFF] rounded-[0.62rem] px-[2.81rem] py-4 font-bold leading-7 text-base tracking-[0.2px]'>App store</button>
 </div>
